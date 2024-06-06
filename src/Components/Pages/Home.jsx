344a68e4cbcfaa1f375d372c656fc7a03568cbfb
Aaditya Jain <aadityajain017@gmail.com>
2024-06-06 14:56:00 +0530

    updated home page

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -3,8 +3,19 @@ import Hovercard from "../ui/HoverCard";
 export const Home = () => {
   return (
     <>
-      <Hovercard />
-      <Hovercard />
+      <Hovercard
+        src="https://img.icons8.com/?size=100&id=tGvHBPJaKqEd&format=png&color=000000"
+        alt="js"
+        title="Javascript"
+        href="/javascript"
+      />
+      <Hovercard
+        src="https://img.icons8.com/?size=100&id=Rc0Xn5AtE8kX&format=png&color=000000"
+        alt="python"
+        title="Python"
+        href="/python"
+      />
+      <div>Select language to learn</div>
     </>
   );
 };
--- src/Components/ui/HoverCard.jsx
+++ src/Components/ui/HoverCard.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 import "./HoverCard.css";
 
-function HoverCard() {
+function HoverCard({ src, alt, title, href }) {
   return (
-    <div className="container noselect">
+    <div
+      className="container noselect"
+      onClick={() => {
+        window.location.href = href;
+      }}
+    >
       <div className="canvas">
         {[...Array(25)].map((_, i) => (
           <div key={i} className={`tracker tr-${i + 1}`}></div>
         ))}
-        <div id="card">
 
+        <div id="card">
           <p id="prompt">
-            <img src="https://img.icons8.com/?size=100&id=tGvHBPJaKqEd&format=png&color=000000" alt="js"></img>
+            <img src={src} alt={alt}></img>
           </p>
 
-          <div className="title">JS</div>
-          <div className="subtitle">Select language to learn</div>
+          <div className="title">{title}</div>
         </div>
       </div>
     </div>
